d1c88bf3dcabaec2a6691a15c3898c8f994c32c2
JustTrying652 <ngataevans@gmail.com>
2026-09-08 14:38:29 +0300

    Initial setup: Django + Channels + Redis config

---
+++ backend/config/settings.py
@@ -0,0 +1,22 @@
+INSTALLED_APPS = [
+    "daphne",          # must be listed BEFORE django.contrib.staticfiles
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "channels",
+    "game",
+]
+
+ASGI_APPLICATION = "config.asgi.application"
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
