1dbc459c265ec63d215a2cfb858469a8e9bdf58a
JustTrying652 <ngataevans@gmail.com>
2026-09-09 14:04:30 +0300

    error handling

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -55,6 +55,7 @@ CHANNEL_LAYERS = {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
             "hosts": [("127.0.0.1", 6379)],
+            "socket_keepalive": True,
         },
     },
 }
