93ac66575980c48fbd5ecc056cc871f364d7721d
JustTrying652 <ngataevans@gmail.com>
2026-09-11 15:35:58 +0300

    standalone room creation

--- backend/game/views.py
+++ backend/game/views.py
@@ -5,7 +5,7 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 
-from .consumers import RoomConsumer, create_room_state
+from .consumers import RoomConsumer
 
 
 def _generate_room_code(length=6):
