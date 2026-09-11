0b296aaa0931bc222069dd67e11fdef30df636fd
JustTrying652 <ngataevans@gmail.com>
2026-09-11 15:18:21 +0300

    add vite routes to cors

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -16,11 +16,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
     "channels",
     "game",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -67,6 +69,11 @@ DATABASES = {
 }
 
 AUTH_PASSWORD_VALIDATORS = []
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5173",
+    "http://localhost:5173",
+]
 
 LANGUAGE_CODE = "en-us"
 TIME_ZONE = "UTC"
