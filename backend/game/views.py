93cd8690d017ebe61517e9c5303eaa2a0dc26ea9
JustTrying652 <ngataevans@gmail.com>
2026-09-12 12:03:53 +0300

    fetch questions

--- backend/game/views.py
+++ backend/game/views.py
@@ -6,7 +6,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 
 from .consumers import RoomConsumer
-
+from .trivia_api import fetch_questions
 
 def _generate_room_code(length=6):
     return "".join(random.choices(string.ascii_uppercase + string.digits, k=length))
@@ -19,9 +19,9 @@ def create_room(request):
     while code in RoomConsumer.rooms:
         code = _generate_room_code()
 
-    RoomConsumer.rooms[code] = RoomConsumer.create_room_state()
+    questions = fetch_questions(amount=10)
+    RoomConsumer.rooms[code] = RoomConsumer.create_room_state(questions=questions)
     return JsonResponse({"room_code": code})
-
 
 @require_http_methods(["GET"])
 def room_status(request, room_code):
